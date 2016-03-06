497e66da5930fae44a54211785ee530e19f570d1
Anna <verdyanna@gmail.com>
2016-03-06 12:03:34 +0400

    konets 2_3- prohodit

--- test/add_test.py
+++ test/add_test.py
@@ -15,11 +15,11 @@ def app(request):
 def test_(app):
 
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="fffff", header="eeeee", footer="ggggg"))
+        app.group.create(Group(name="fffff", header="eeeee", footer="ggggg"))
         app.session.logout()
 
 def test_emptygroup(app):
 
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
+        app.group.create(Group(name="", header="", footer=""))
         app.session.logout()
