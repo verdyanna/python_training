faae486331da0ed5b7c4e9f27b77ef311b1dd4c0
Anna <verdyanna@gmail.com>
2016-03-11 08:02:03 +0300

    konets 4_1 проходит

--- test/add_test.py
+++ test/add_test.py
@@ -3,9 +3,15 @@
 from model.group import Group
 
 def test_(app):
+        old_groups = app.group.get_group_list()
         app.group.create(Group(name="fffff", header="eeeee", footer="ggggg"))
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_emptygroup(app):
+        old_groups = app.group.get_group_list()
         app.group.create(Group(name="", header="", footer=""))
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
 
