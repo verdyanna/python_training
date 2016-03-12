017f65a717b8471563668dfe4ed68f59b25e449c
Anna <verdyanna@gmail.com>
2016-03-12 08:02:12 +0300

    konets 4_3 проходит

--- test/add_test.py
+++ test/add_test.py
@@ -2,16 +2,23 @@
 
 from model.group import Group
 
+
 def test_(app):
         old_groups = app.group.get_group_list()
-        app.group.create(Group(name="fffff", header="eeeee", footer="ggggg"))
+        group = Group(name="fffff", header="eeeee", footer="ggggg")
+        app.group.create(group)
         new_groups = app.group.get_group_list()
         assert len(old_groups) + 1 == len(new_groups)
+        old_groups.append(group)
+        assert  sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_emptygroup(app):
         old_groups = app.group.get_group_list()
-        app.group.create(Group(name="", header="", footer=""))
+        group = Group(name="", header="", footer="")
+        app.group.create(group)
         new_groups = app.group.get_group_list()
         assert len(old_groups) + 1 == len(new_groups)
+        old_groups.append(group)
+        assert  sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
